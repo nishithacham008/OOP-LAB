53f45e62c4c8e62b9d2da99f7d6fc6c26aefa7bc
nishithacham008 <nishithacham@gmail.com>
2026-03-05 21:05:54 +0530

    Delete Basic Java Programs directory

--- Basic Java Programs/Concatenation.java.java
+++
@@ -1,9 +0,0 @@
-public class Concatenation {
-  public static void main(String[] args) {
-    String name = "Ruby";
-    int x = 2;
-    String y = "1";
-    String age = x + y;
-    System.out.println("Name:"+name+" "+"Age:"+age);
-  }
-}
