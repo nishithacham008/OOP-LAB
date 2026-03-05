c50732dcdd8af57ce44a49b7ed445244f6a773ee
nishithacham008 <nishithacham@gmail.com>
2026-03-05 21:00:31 +0530

    Add files via upload

---
+++ Basic Java Programs/Concatenation.java.java
@@ -0,0 +1,9 @@
+public class Concatenation {
+  public static void main(String[] args) {
+    String name = "Ruby";
+    int x = 2;
+    String y = "1";
+    String age = x + y;
+    System.out.println("Name:"+name+" "+"Age:"+age);
+  }
+}
